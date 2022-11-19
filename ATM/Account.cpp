edbab6cbdaec8147bbd63064039be98838973b34
dieumerci7 <77683495+dieumerci7@users.noreply.github.com>
2022-11-19 11:37:18 +0200

    Account classes
    Realization of main methods (deposit, withdraw, and transfer). Withdrawals aren't yet account-specific.

---
+++ ATM/Account.cpp
@@ -0,0 +1,76 @@
+#include "Account.h"
+#include <QDebug>
+
+Account::Account(const QString& number, const QString& pincode, const QString& phone, double initBalance) :
+    _number(number), _pincode(pincode), _phone(phone), _balance(initBalance) {
+    if (initBalance < 0) {
+        throw BadAccount("Initial balance < 0 at Account::Account");
+    }
+}
+
+void Account::withdraw(double sum) {
+    doWithdraw(sum);
+}
+
+void Account::deposit(double sum) {
+    doDeposit(sum);
+}
+
+void Account::transfer(Account& to, double sum) {
+    doTransfer(to, sum);
+}
+
+double Account::balance() const {
+    return _balance;
+}
+
+const QString& Account::number() const {
+    return _number;
+}
+
+const QString& Account::pincode() const {
+    return _pincode;
+}
+
+void Account::setPincode(const QString&) {
+    doSetPincode();
+}
+
+const QString& Account::phone() const {
+    return _phone;
+}
+
+void Account::doWithdraw(double sum) {
+    if (sum < 0) {
+        throw BadAccount("Negative argument at Account::doWithdraw");
+    }
+    if (sum > _balance) {
+        throw BadAccount("Attempt to withdraw sum greater than balance at Account::doWithdraw");
+    }
+    _balance -= sum;
+}
+
+void Account::doDeposit(double sum) {
+    if (sum < 0) {
+        throw BadAccount("Negative argument at Account::doDeposit");
+    }
+    _balance += sum;
+}
+
+void Account::doTransfer(Account& to, double sum) {
+    if (sum < 0) {
+        throw BadAccount("Negative argument at Account::doTransfer");
+    }
+    if (sum > _balance) {
+        throw BadAccount("Attempt to withdraw sum greater than balance at Account::doTransfer");
+    }
+    withdraw(sum);
+    to.deposit(sum);
+}
+
+Account::BadAccount::BadAccount(const QString& errorMessage) :
+    _errorMessage(errorMessage) {}
+
+void Account::BadAccount::showMessage() const {
+    qDebug() <<_errorMessage;
+}
--- ATM/main.cpp
+++ ATM/main.cpp
@@ -1,11 +1,26 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QDebug>
+
+#include "CheckingAccount.h"
+#include "SavingsAccount.h"
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    SavingsAccount sa("2222222222222222", "0000", "+380977439838", 10000);
+    CheckingAccount ca("1111111111111111", "1111", "+380994720018", 900, 1000, &sa);
+    //ca.withdraw(400);
+    ca.deposit(500);
+    qDebug() << ca.balance();
+    qDebug() << sa.balance();
+    ca.transfer(sa, 500);
+    qDebug() << ca.balance();
+    qDebug() << sa.balance();
     return a.exec();
 }
